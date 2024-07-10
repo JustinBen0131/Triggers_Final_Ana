a5b0e7b1c9fa9fb2b3f6b84e137149599c8fc269
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2024-07-10 12:54:28 -0500

    Create triggerCondor_submit.sh

---
+++ condor/triggerCondor_submit.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# List of run numbers
+runNumbers=("47495" "47501" "47502" "47503" "47506" "47509" "47516" "47517" "47521" "47524" "47540" "47549" "47552" "47557" "47634" "47635" "47637")
+
+# Loop over each run number and create a custom submission file
+for runNumber in "${runNumbers[@]}"; do
+  # Create a custom submission file for the current run number
+  cat > triggerCondor_${runNumber}.sub <<EOL
+universe                = vanilla
+executable              = triggerCondor.sh
+arguments               = ${runNumber} \$(filename) \$(Cluster)
+log                     = log/job.\$(Cluster).\$(Process).log
+output                  = stdout/job.\$(Cluster).\$(Process).out
+error                   = error/job.\$(Cluster).\$(Process).err
+request_memory          = 7GB
+queue filename from dst_calo_run2pp-000${runNumber}.list
+EOL
+
+  # Submit the job
+  condor_submit triggerCondor_${runNumber}.sub
+done
